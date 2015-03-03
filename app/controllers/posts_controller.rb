c0dec9bfc1456f9ab61c5bc2bc5002c5713654a5
intheblackworld <intheblackworld@gmail.com>
2015-03-03 12:39:15 +0800

    add Post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+
+  end
+
+  def update
+  
+  end
+
+  def destroy
+  
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
